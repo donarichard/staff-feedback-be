2342aee6c8c94530fa8d6186c4aad44c9667bfdf
Dona <donarichard48@gmail.com>
2023-11-08 11:31:04 +0530

    Updated the babel config

---
+++ .babelrc.js
@@ -0,0 +1,14 @@
+
+module.exports = {
+    presets: [
+      [
+        '@babel/preset-env',
+        {
+          targets: {
+            node: 'current',
+          },
+        },
+      ],
+    ],
+    ignore: ['node_modules', 'dist'],
+  };
